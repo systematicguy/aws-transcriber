1fa06fec1d1fe7aa7f4d2ca8a5aba9106ccc8952
David Horvath <david.h@systematicguy.com>
2024-09-14 20:55:41 +0200

    move file, start transcription

---
+++ lib/lambda/process_uploaded_audio/process_uploaded_audio.py
@@ -0,0 +1,67 @@
+# MIT License
+# Copy it, but attribute the author: David Horvath - systematicguy.com
+# Linking to the repo is also fine.
+
+import os
+import json
+from pathlib import Path
+from datetime import datetime
+from zoneinfo import ZoneInfo
+
+import boto3
+
+
+JOB_INPUT_BUCKET = os.getenv('JOB_INPUT_BUCKET')
+
+# Supported media formats for Amazon Transcribe
+SUPPORTED_FORMATS = ['mp3', 'mp4', 'wav', 'flac', 'ogg', 'amr', 'webm', 'm4a']
+
+s3 = boto3.client('s3')
+
+TIMEZONE = os.getenv('TIMEZONE', 'UTC')
+timezone_preference = ZoneInfo(TIMEZONE)
+
+
+def handler(event, context):
+    # Extract bucket name and object key from the event
+    bucket = event['Records'][0]['s3']['bucket']['name']
+    key = event['Records'][0]['s3']['object']['key']
+    event_datetime_str = event['Records'][0]['eventTime']
+
+    print(f"{event_datetime_str=}")
+    event_datetime = datetime.fromisoformat(event_datetime_str)
+
+    file_extension = Path(key).suffix.lower().strip('.')
+    print(f"{file_extension=}")
+
+    if file_extension not in SUPPORTED_FORMATS:
+        raise ValueError(f"Unsupported file format: {file_extension}, supported formats: {SUPPORTED_FORMATS}")
+
+    # Generate new key with suffix based on upload time in the desired timezone as per TZ
+    datetime_suffix = event_datetime.astimezone(timezone_preference).strftime('%Y-%m-%d-%H-%M-%S')
+    new_s3_key = f"{Path(key).stem}-uploaded-{datetime_suffix}.{file_extension}"
+
+    # Copy the file to the audio bucket with the new key
+    s3.copy_object(
+        Bucket=JOB_INPUT_BUCKET,
+        CopySource={'Bucket': bucket, 'Key': key},
+        Key=new_s3_key
+    )
+
+    # Delete the original file from the upload bucket
+    s3.delete_object(Bucket=bucket, Key=key)
+
+    # Prepare the response payload for Step Function
+    response = {
+        "TranscriptionJobName": new_s3_key,
+        "Media": {
+            "MediaFileUri": f"s3://{JOB_INPUT_BUCKET}/{new_s3_key}",
+        },
+        "MediaFormat": file_extension,
+        "OutputKey": f"{new_s3_key}-transcription.json"
+    }
+
+    return {
+        'statusCode': 200,
+        'body': json.dumps(response)
+    }
--- lib/transcriber-stack.ts
+++ lib/transcriber-stack.ts
@@ -4,8 +4,19 @@
  */
 
 import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
+import {Construct} from 'constructs';
 import * as s3 from 'aws-cdk-lib/aws-s3';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as path from 'path';
+import * as iam from 'aws-cdk-lib/aws-iam';
+import * as tasks from 'aws-cdk-lib/aws-stepfunctions-tasks';
+import * as events from 'aws-cdk-lib/aws-events';
+import * as targets from 'aws-cdk-lib/aws-events-targets';
+import * as stepfunctions from 'aws-cdk-lib/aws-stepfunctions';
+
+
+
+const TIMEZONE = 'Europe/Zurich';
 
 export class TranscriberStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -13,8 +24,104 @@ export class TranscriberStack extends cdk.Stack {
 
     const prefixedName = "transcriber-dev";
 
-    const inputBucket = new s3.Bucket(this, 'AudioInputBucket', {
-      bucketName: `${prefixedName}-${this.account}-input`,
+    const uploadBucket = new s3.Bucket(this, 'UserUploadBucket', {
+      bucketName: `${prefixedName}-${this.account}-user-upload`,
+    });
+
+    const audioBucket = new s3.Bucket(this, 'AudioInputBucket', {
+      bucketName: `${prefixedName}-${this.account}-audio-input`,
+    });
+
+    const outputBucket = new s3.Bucket(this, 'TranscriptionOutputBucket', {
+      bucketName: `${prefixedName}-${this.account}-transcription-output`,
+    });
+
+    // *****************************************************************************************************************
+    // Lambda Function to process uploaded files
+    const processFileLambda = new lambda.Function(this, 'ProcessUploadedAudioFileLambda', {
+      functionName: `${prefixedName}-process-uploaded-audio`,
+      runtime: lambda.Runtime.PYTHON_3_12,
+      code: lambda.Code.fromAsset(path.join(__dirname, 'lambda/process_uploaded_audio/')),
+      handler: 'process_uploaded_audio.handler',
+      environment: {
+        JOB_INPUT_BUCKET: audioBucket.bucketName,
+        TIMEZONE: TIMEZONE,
+      },
+    });
+
+    uploadBucket.grantReadWrite(processFileLambda);
+    audioBucket.grantReadWrite(processFileLambda);
+
+    // *****************************************************************************************************************
+    // step function
+    const transcribeRole = new iam.Role(this, 'TranscribeRole', {
+      assumedBy: new iam.ServicePrincipal('states.amazonaws.com'),
+    });
+
+    transcribeRole.addToPolicy(new iam.PolicyStatement({
+      actions: [
+        'transcribe:StartTranscriptionJob',
+        'transcribe:GetTranscriptionJob'
+      ],
+      resources: ['*'],
+    }));
+    outputBucket.grantReadWrite(transcribeRole);
+
+    // Step Function Task: Lambda to Process File
+    const processFileTask = new tasks.LambdaInvoke(this, 'ProcessUploadedFile', {
+      lambdaFunction: processFileLambda,
+      outputPath: '$.Payload', // Extracts the payload from the Lambda's response
+    });
+
+    // https://docs.aws.amazon.com/transcribe/latest/APIReference/API_StartTranscriptionJob.html
+    const startTranscriptionTask = new tasks.CallAwsService(this, 'StartTranscription', {
+      service: 'transcribe',
+      action: 'startTranscriptionJob',
+      parameters: {
+        // TODO: take all field from the lambda output
+
+        // map lambda output to the parameters
+        TranscriptionJobName: stepfunctions.JsonPath.stringAt('$.TranscriptionJobName'),
+        MediaFormat: stepfunctions.JsonPath.stringAt('$.MediaFormat'),
+        Media: {
+          MediaFileUri: stepfunctions.JsonPath.stringAt('$.Media.MediaFileUri'),
+        },
+        OutputKey: stepfunctions.JsonPath.stringAt('$.OutputKey'),
+
+        // ----------------------------------------------
+        // lambda-independent parameters
+        // https://docs.aws.amazon.com/transcribe/latest/APIReference/API_StartTranscriptionJob.html#transcribe-StartTranscriptionJob-request-IdentifyMultipleLanguages
+        IdentifyMultipleLanguages: true,
+        LanguageOptions: ['en-US', 'hu-HU'],
+
+        // https://docs.aws.amazon.com/transcribe/latest/APIReference/API_StartTranscriptionJob.html#transcribe-StartTranscriptionJob-request-OutputBucketName
+        OutputBucketName: outputBucket.bucketName,
+      },
+      iamResources: ['*'], // TODO check how this works
+    });
+
+    const taskChain = processFileTask
+      .next(startTranscriptionTask);
+
+    // Create the State Machine
+    const stateMachine = new stepfunctions.StateMachine(this, 'TranscriptionStateMachine', {
+      stateMachineName: prefixedName,
+      definitionBody: stepfunctions.DefinitionBody.fromChainable(taskChain),
+      role: transcribeRole,
+    });
+
+    // EventBridge Rule to trigger the Step Function
+    new events.Rule(this, 'OnFileUpload', {
+      eventPattern: {
+        source: ['aws.s3'],
+        detailType: ['Object Created'],
+        detail: {
+          bucket: {
+            name: [uploadBucket.bucketName],
+          },
+        },
+      },
+      targets: [new targets.SfnStateMachine(stateMachine)],
     });
   }
 }
